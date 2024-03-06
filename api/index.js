0333fec117086cfd584ffade7cacdbe4622bbc9e
shortcentury <tsaridakis@gmail.com>
2024-03-06 23:23:48 +0200

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("MongoDB is connected!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
