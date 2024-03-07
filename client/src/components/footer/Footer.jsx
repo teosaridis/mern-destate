01d7be35608c86876a74cfd14348fd7189f9c28b
shortcentury <tsaridakis@gmail.com>
2024-03-07 18:09:21 +0200

    Create signin api route

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -11,36 +11,36 @@ import {
 
 export default function FooterComp() {
   return (
-    <Footer container className="border border-t-8">
+    <Footer container className="border border-t-8 border-primary">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1 ">
           <div className="mt-5">
             <Link
               to={"/"}
-              className="text-sm self-center whitespace-nowrap sm:text-xl font-semibold"
+              className="text-lg self-center whitespace-nowrap sm:text-xl font-semibold"
             >
-              <span className="px-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                 Teos
               </span>
               Blog
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
-            <div className="">
+            <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
                 <Footer.Link>Disclaimer</Footer.Link>
                 <Footer.Link>Contact</Footer.Link>
               </Footer.LinkGroup>
             </div>
-            <div className="">
+            <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
                 <Footer.Link>Disclaimer</Footer.Link>
                 <Footer.Link>Contact</Footer.Link>
               </Footer.LinkGroup>
             </div>
-            <div className="">
+            <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
                 <Footer.Link>Disclaimer</Footer.Link>
@@ -50,13 +50,15 @@ export default function FooterComp() {
           </div>
         </div>
         <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright
-            href="#"
-            by="Teo's Blog"
-            year={new Date().getFullYear()}
-          />
-          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+        <div className="w-full sm:flex items-center sm:justify-between">
+          <div className="flex gap-6 sm:mt-0 mt-4 justify-center">
+            <Footer.Copyright
+              href="#"
+              by="Teo's Blog"
+              year={new Date().getFullYear()}
+            />
+          </div>
+          <div className="flex gap-6 sm:mt-0 mt-4 justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
