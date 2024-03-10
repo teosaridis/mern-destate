a2787ae112261179f4a14e5164f0f97748723376
shortcentury <tsaridakis@gmail.com>
2024-03-10 18:56:00 +0200

    Add Redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+
 import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
