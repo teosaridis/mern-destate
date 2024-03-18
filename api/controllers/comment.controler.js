751b5a4fbc0d876ee3ad79d223ba69272e46744c
shortcentury <tsaridakis@gmail.com>
2024-03-19 00:25:41 +0200

    add Show comments to the post page

--- api/controllers/comment.controler.js
+++ api/controllers/comment.controler.js
@@ -15,3 +15,15 @@ export const createComment = async (req, res, next) => {
     res.status(200).json(newComment);
   } catch (error) {}
 };
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/comment.controler.js";
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/comment.controler.js";
 
 const router = express.Router();
 
 router.post(`/create`, verifyToken, createComment);
+router.get(`/get-post-comments/:postId`, getPostComments);
 
 export default router;
